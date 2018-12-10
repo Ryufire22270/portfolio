55d273abde3a727e8af4c3e78c02eb3869bc4ec1
Ryufire22270 <mjacobs21321@gmail.com>
2018-12-10 10:51:54 -0500

    Created Header component and React structure for site, created initial structure for App.js

---
+++ client/src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+       <nav>
+        <ul className='user-list'>
+          <li className="user-link"><Link to='/contactme'>Contact me</Link></li>
+        </ul>
+        <img className="TBA" src="https://www.fillmurray.com/g/200/300" title="source: fillmurray.com" />
+        <ul className='home-list'>
+          <li className="home-links"><Link to='/'>Home</Link></li>
+          <li className="home-links"><Link to='/aboutme'>About Me</Link></li>
+          <li className="home-links"><Link to='/floorplan'>Map</Link></li>
+        </ul>
+      </nav>
+    </header>
+
+    )
+}
+
+export default Header;
