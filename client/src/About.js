7305719a936f33041dccff238e1a7e01639983e6
Ryufire22270 <mjacobs21321@gmail.com>
2018-12-10 11:43:40 -0500

    flushed out about me page and fixed header to be neater for styling

--- client/src/About.js
+++ client/src/About.js
@@ -6,6 +6,12 @@ class About extends Component {
     return(
         <div className="about-me">
           <Header />
+          <h1 className="title">About Me</h1>
+          <img className="headshot" src="https://i.imgur.com/Iefe4ij.jpg" title="source imgur.com"
+          <br />
+          <h2 className="name">Matthew Jacobs<h2>
+          <h2 className="summary"><h2>
+          <h2 className="useful-link"><h2>
         </div>
       )
   }
--- client/src/Header.js
+++ client/src/Header.js
@@ -5,14 +5,12 @@ function Header() {
   return (
     <header>
        <nav>
-        <ul className='user-list'>
-          <li className="user-link"><Link to='/contactme'>Contact me</Link></li>
-        </ul>
         <img className="TBA" src="https://www.fillmurray.com/g/200/300" title="source: fillmurray.com" />
         <ul className='home-list'>
           <li className="home-links"><Link to='/'>Home</Link></li>
           <li className="home-links"><Link to='/aboutme'>About Me</Link></li>
           <li className="home-links"><Link to='/projects'>My Projects</Link></li>
+          <li className="home-link"><Link to='/contactme'>Contact me</Link></li>
         </ul>
       </nav>
     </header>
