3120020eb9188e4e01d0a9b3591f08629e77e347
Ryufire22270 <mjacobs21321@gmail.com>
2018-12-12 13:54:45 -0500

    added brand statement to about me page

--- client/src/About.js
+++ client/src/About.js
@@ -10,8 +10,7 @@ class About extends Component {
           <img className="headshot" src="https://i.imgur.com/Iefe4ij.jpg" title="source imgur.com"
           <br />
           <h2 className="name">Matthew Jacobs<h2>
-          <h2 className="summary"><h2>
-          <h2 className="useful-link"><h2>
+          <h2 className="summary">As a web developer who holds over four years experience in education sales, I am an expert at diagnosing the root cause of any clients’ issue and developing customized solutions that speak to the heart of their needs. Outgoing, sociable, and innovative, I love meeting new people across all industries and working with collaboratively to break down complicated problems, into intuitive solutions. I strive to combine my comprehensible logic with clean code in order to exceed both internal and client expectations in every challenge I face.<h2>
         </div>
       )
   }
