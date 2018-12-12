2609f45af753ce2cbacd223bfdb21134a8c4a702
Ryufire22270 <mjacobs21321@gmail.com>
2018-12-12 16:50:27 -0500

    done styling About and Contact still need horizontal lines for page

--- client/src/About.js
+++ client/src/About.js
@@ -4,13 +4,15 @@ import Header from './Header';
 class About extends Component {
   render() {
     return(
-        <div className="about-me">
+        <div className="about-me-page">
           <Header />
-          <h1 className="title">About Me</h1>
+          <h1 className="about-title">About Me</h1>
+          <h2 className="about-name">Matthew Jacobs</h2>
           <br />
-          <img className="headshot" src="https://i.imgur.com/Iefe4ij.jpg" title="source imgur.com" alt="" />
-          <h2 className="name">Matthew Jacobs</h2>
-          <h2 className="summary">As a web developer who holds over four years experience in education sales, I am an expert at diagnosing the root cause of any clients’ issue and developing customized solutions that speak to the heart of their needs. Outgoing, sociable, and innovative, I love meeting new people across all industries and working with collaboratively to break down complicated problems, into intuitive solutions. I strive to combine my comprehensible logic with clean code in order to exceed both internal and client expectations in every challenge I face.</h2>
+          <div className="about-me-single">
+            <img className="about-headshot" src="https://i.imgur.com/Iefe4ij.jpg" title="source imgur.com" alt="" />
+            <h2 className="about-summary">As a web developer who holds over four years experience in education sales, I am an expert at diagnosing the root cause of any clients’ issue and developing customized solutions that speak to the heart of their needs. Outgoing, sociable, and innovative, I love meeting new people across all industries and working with collaboratively to break down complicated problems, into intuitive solutions. I strive to combine my comprehensible logic with clean code in order to exceed both internal and client expectations in every challenge I face.</h2>
+          </div>
         </div>
       )
   }
--- client/src/Contact.js
+++ client/src/Contact.js
@@ -4,18 +4,20 @@ import Header from './Header';
 class Contact extends Component {
   render() {
     return(
-        <div className="contact-me">
+        <div className="contact-me-page">
           <Header />
-          <h1 className="contact-title">Contact Me</h1>
+            <h1 className="contact-title">Contact Me</h1>
           <br />
-          <img className="headshot" src="https://i.imgur.com/Iefe4ij.jpg" title="source imgur.com" alt="" />
-          <h2 className="contact-name">Matthew Jacobs</h2>
-          <h2 className="contact-job">Web Developer</h2>
-          <h2 className="contact-location">Greater New York Area</h2>
-          <h2 className="contact-phone">(973)-715-5800</h2>
-          <h2 className="contact-email">mjacobs21321@gmail.com</h2>
-          <h2 className="contact-git">github.com/Ryufire22270</h2>
-          <h2 className="contact-linkedin">linkedin.com/in/matthewjacobs21/</h2>
+            <div className="contact-me-single">
+              <img className="contact-headshot" src="https://i.imgur.com/Iefe4ij.jpg" title="source imgur.com" alt="" />
+              <h2 className="contact-name">Matthew Jacobs</h2>
+              <h2 className="contact-job">Web Developer</h2>
+              <h2 className="contact-location">Greater New York Area</h2>
+              <h2 className="contact-phone">(973)-715-5800</h2>
+              <h2 className="contact-email">mjacobs21321@gmail.com</h2>
+              <h2 className="contact-git">github.com/Ryufire22270</h2>
+              <h2 className="contact-linkedin">linkedin.com/in/matthewjacobs21/</h2>
+            </div>
         </div>
       )
   }
